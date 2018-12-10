ad9146b84152b4a6597b0177457ab2c8874173f9
jesseh21 <jjhong89@gmail.com>
2018-12-10 18:23:35 +0900

    Genius | National Geographic

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,23 +1,125 @@
-$(function(){
-    //$("#dialog").dialog();
-    $("#dialog").dialog({
-        autoOpen:false, //자동으로 열리지않게
-        position:[100,200], //x,y  값을 지정
-        //"center", "left", "right", "top", "bottom"
-        modal:true, //모달대화상자
-        resizable:false, //크기 조절 못하게
-        
-        buttons:{
-            "확인":function(){
-                $(this).dialog("close");
-            },"취소":function(){
-                $(this).dialog("close");
-            }
-        }
+;
+(function() {
+
+  var Modal = function() {
+
+    var prefix = 'Modal-';
+
+    this.Class = {
+      stopOverflow: prefix + 'cancel-overflow',
+      overlay: prefix + 'overlay',
+      box: prefix + 'box',
+      close: prefix + 'close'
+    };
+
+    this.Selector = {
+      overlay: '.' + this.Class.overlay,
+      box: '.' + this.Class.box,
+      button: '[data-modal=button]'
+    };
+
+    this.Markup = {
+      close: '<div class=" ' + this.Class.close + ' ">Close X</div>',
+      overlay: '<div class=" ' + this.Class.overlay + ' "></div>',
+      box: '<div class=" ' + this.Class.box + ' "></div>'
+    }
+
+    this.youtubeID = false;
+
+  };
+
+  Modal.prototype = {
+
+    toggleOverflow: function() {
+      $('body').toggleClass(this.Class.stopOverflow);
+    },
+
+    videoContainer: function() {
+      return '<div class="video-container"><iframe id="player" src="https://www.youtube.com/embed/' + this.youtubeID + '?autoplay=1&rel=0" frameborder="0"></iframe></div>';
+    },
+
+    addOverlay: function() {
+
+      var self = this;
+
+      $(this.Markup.overlay).appendTo('body').fadeIn('slow', function() {
+        self.toggleOverflow();
+      });
+
+      $(this.Selector.overlay).on('click touchstart', function() {
+        self.closeModal();
+      })
+
+    },
+
+    addModalBox: function() {
+      $(this.Markup.box).appendTo(this.Selector.overlay);
+    },
+
+    buildModal: function(youtubeID) {
+
+      this.addOverlay();
+      this.addModalBox();
+
+      $(this.Markup.close).appendTo(this.Selector.overlay);
+      $(this.videoContainer(youtubeID)).appendTo(this.Selector.box);
+
+    },
+
+    closeModal: function() {
+
+      this.toggleOverflow();
+
+      $(this.Selector.overlay).fadeOut().detach();
+      $(this.Selector.box).empty();
+
+    },
+
+    getYoutubeID: function() {
+      return this.youtubeID;
+    },
+
+    setYoutubeID: function(href) {
+
+      var id = '';
+
+      if (href.indexOf('youtube.com') > -1) {
+        // full Youtube link
+        id = href.split('v=')[1];
+      } else if (href.indexOf('youtu.be') > -1) {
+        // shortened Youtube link
+        id = href.split('.be/')[1];
+      } else {
+        // in case it's not a Youtube link, send them on their merry way
+        document.location = href;
+      }
+
+      // If there's an ampersand, remove it and return what's left, otherwise return the ID
+      this.youtubeID = (id.indexOf('&') != -1) ? id.substring(0, amp) : id;
+
+    },
+
+    startup: function(href) {
+
+      this.setYoutubeID(href);
+
+      if (this.youtubeID) {
+        this.buildModal();
+      }
+
+    }
+
+  };
+
+  $(document).ready(function() {
+
+    var modal = new Modal();
+
+    $(modal.Selector.button).on('click touchstart', function(e) {
+      e.preventDefault();
+      modal.startup(this.href);
     });
 
-    //창 열기 버튼을 클릭했을경우
-    $("#btn").on("click",function(){
-        $("#dialog").dialog("open"); //다이얼로그창 오픈                
-    });
-});
+  });
+
+})(this);
