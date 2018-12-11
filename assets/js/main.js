72a558d91315d52813b0fbd2631eab34a337d2c2
jesseh21 <jjhong89@gmail.com>
2018-12-11 13:10:13 +0900

    Genius | National Geographic

--- assets/js/main.js
+++ assets/js/main.js
@@ -352,39 +352,4 @@
 
 })(jQuery);
 
-// find the modal & close button
-var modal = document.getElementById('RFImodal'),
-    close = modal.querySelectorAll('article button')[0],
-    modalStatus = false;
-// how to add the modal to the page
-function spawnModal(e){
-  var wistiaEmbed = document.querySelectorAll('.wistia_embed')[0];
-  modalStatus = true;
-  modal.style.pointerEvents = 'auto';
-  wistiaEmbed.style.display = 'block';
-  modal.style.opacity = '1';
-  wistiaEmbed.wistiaApi.play();
-}
-// teach the modal backdrop and close button to close the modal
-modal.addEventListener('click',clickClose);
-close.addEventListener('click',clickClose);
-function clickClose(e){
-  if (e.target == modal || e.target == close){
-    killModal()
-  }
-}
-// teach the 'escape' key to close the modal
-document.onkeyup = function(e) {
-  e = e || window.event;
-  var charCode = (typeof e.which == "number")?e.which:e.keyCode;
-  if (charCode == 27 && modalStatus == true) { killModal() }
-}
-// how to close the modal
-function killModal(){
-  var wistiaEmbed = document.querySelectorAll('.wistia_embed')[0];
-  wistiaEmbed.wistiaApi.pause();
-  modalStatus = false;
-  modal.style.pointerEvents = 'none';
-  modal.style.opacity = '0';
-  wistiaEmbed.style.display = 'none';
-}
+
--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,125 +1,35 @@
-
-(function() {
-
-  var Modal = function() {
-
-    var prefix = 'Modal-';
-
-    this.Class = {
-      stopOverflow: prefix + 'cancel-overflow',
-      overlay: prefix + 'overlay',
-      box: prefix + 'box',
-      close: prefix + 'close'
-    };
-
-    this.Selector = {
-      overlay: '.' + this.Class.overlay,
-      box: '.' + this.Class.box,
-      button: '[data-modal=button]'
-    };
-
-    this.Markup = {
-      close: '<div class=" ' + this.Class.close + ' ">Close X</div>',
-      overlay: '<div class=" ' + this.Class.overlay + ' "></div>',
-      box: '<div class=" ' + this.Class.box + ' "></div>'
-    }
-
-    this.youtubeID = false;
-
-  };
-
-  Modal.prototype = {
-
-    toggleOverflow: function() {
-      $('body').toggleClass(this.Class.stopOverflow);
-    },
-
-    videoContainer: function() {
-      return '<div class="video-container"><iframe id="player" src="https://www.youtube.com/embed/' + this.youtubeID + '?autoplay=1&rel=0" frameborder="0"></iframe></div>';
-    },
-
-    addOverlay: function() {
-
-      var self = this;
-
-      $(this.Markup.overlay).appendTo('body').fadeIn('slow', function() {
-        self.toggleOverflow();
-      });
-
-      $(this.Selector.overlay).on('click touchstart', function() {
-        self.closeModal();
-      })
-
-    },
-
-    addModalBox: function() {
-      $(this.Markup.box).appendTo(this.Selector.overlay);
-    },
-
-    buildModal: function(youtubeID) {
-
-      this.addOverlay();
-      this.addModalBox();
-
-      $(this.Markup.close).appendTo(this.Selector.overlay);
-      $(this.videoContainer(youtubeID)).appendTo(this.Selector.box);
-
-    },
-
-    closeModal: function() {
-
-      this.toggleOverflow();
-
-      $(this.Selector.overlay).fadeOut().detach();
-      $(this.Selector.box).empty();
-
-    },
-
-    getYoutubeID: function() {
-      return this.youtubeID;
-    },
-
-    setYoutubeID: function(href) {
-
-      var id = '';
-
-      if (href.indexOf('youtube.com') > -1) {
-        // full Youtube link
-        id = href.split('v=')[1];
-      } else if (href.indexOf('youtu.be') > -1) {
-        // shortened Youtube link
-        id = href.split('.be/')[1];
-      } else {
-        // in case it's not a Youtube link, send them on their merry way
-        document.location = href;
-      }
-
-      // If there's an ampersand, remove it and return what's left, otherwise return the ID
-      this.youtubeID = (id.indexOf('&') != -1) ? id.substring(0, amp) : id;
-
-    },
-
-    startup: function(href) {
-
-      this.setYoutubeID(href);
-
-      if (this.youtubeID) {
-        this.buildModal();
-      }
-
-    }
-
-  };
-
-  $(document).ready(function() {
-
-    var modal = new Modal();
-
-    $(modal.Selector.button).on('click touchstart', function(e) {
-      e.preventDefault();
-      modal.startup(this.href);
-    });
-
-  });
-
-})(this);
+// find the modal & close button
+var modal = document.getElementById('RFImodal'),
+    close = modal.querySelectorAll('article button')[0],
+    modalStatus = false;
+// how to add the modal to the page
+function spawnModal(e){
+  var wistiaEmbed = document.querySelectorAll('.wistia_embed')[0];
+  modalStatus = true;
+  modal.style.pointerEvents = 'auto';
+  wistiaEmbed.style.display = 'block';
+  modal.style.opacity = '1';
+  wistiaEmbed.wistiaApi.play();
+}
+// teach the modal backdrop and close button to close the modal
+modal.addEventListener('click',clickClose);
+close.addEventListener('click',clickClose);
+function clickClose(e){
+  if (e.target == modal || e.target == close){
+    killModal()
+  }
+}
+// teach the 'escape' key to close the modal
+document.onkeyup = function(e) {
+  e = e || window.event;
+  var charCode = (typeof e.which == "number")?e.which:e.keyCode;
+  if (charCode == 27 && modalStatus == true) { killModal() }
+}
+// how to close the modal
+function killModal(){
+  var wistiaEmbed = document.querySelectorAll('.wistia_embed')[0];
+  wistiaEmbed.wistiaApi.pause();
+  modalStatus = false;
+  modal.style.pointerEvents = 'none';
+  modal.style.opacity = '0';
+  wistiaEmbed.style.display = 'none';
