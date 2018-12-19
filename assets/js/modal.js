8cb5869776daed48c97b9e66405a1ed3a4718714
jesseh21 <jjhong89@gmail.com>
2018-12-19 17:09:14 +0900

    Genius | National Geographic

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,35 +1,23 @@
-// find the modal & close button
-var modal = document.getElementById('RFImodal'),
-    close = modal.querySelectorAll('article button')[0],
-    modalStatus = false;
-// how to add the modal to the page
-function spawnModal(e){
-  var wistiaEmbed = document.querySelectorAll('.wistia_embed')[0];
-  modalStatus = true;
-  modal.style.pointerEvents = 'auto';
-  wistiaEmbed.style.display = 'block';
-  modal.style.opacity = '1';
-  wistiaEmbed.wistiaApi.play();
-}
-// teach the modal backdrop and close button to close the modal
-modal.addEventListener('click',clickClose);
-close.addEventListener('click',clickClose);
-function clickClose(e){
-  if (e.target == modal || e.target == close){
-    killModal()
+window.document.onkeydown = function(e) {
+  if (!e) {
+    e = event;
+  }
+  if (e.keyCode == 27) {
+    lightbox_close();
   }
 }
-// teach the 'escape' key to close the modal
-document.onkeyup = function(e) {
-  e = e || window.event;
-  var charCode = (typeof e.which == "number")?e.which:e.keyCode;
-  if (charCode == 27 && modalStatus == true) { killModal() }
+
+function lightbox_open() {
+  var lightBoxVideo = document.getElementById("VisaChipCardVideo");
+  window.scrollTo(0, 0);
+  document.getElementById('light').style.display = 'block';
+  document.getElementById('fade').style.display = 'block';
+  lightBoxVideo.play();
 }
-// how to close the modal
-function killModal(){
-  var wistiaEmbed = document.querySelectorAll('.wistia_embed')[0];
-  wistiaEmbed.wistiaApi.pause();
-  modalStatus = false;
-  modal.style.pointerEvents = 'none';
-  modal.style.opacity = '0';
-  wistiaEmbed.style.display = 'none';
+
+function lightbox_close() {
+  var lightBoxVideo = document.getElementById("VisaChipCardVideo");
+  document.getElementById('light').style.display = 'none';
+  document.getElementById('fade').style.display = 'none';
+  lightBoxVideo.pause();
+}
